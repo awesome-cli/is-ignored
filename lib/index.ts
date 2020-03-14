dd8c43af6a38321c8b10f9b24018f7e3719d8452
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-14 12:23:44 +0100

    Update commander

--- lib/index.ts
+++ lib/index.ts
@@ -16,14 +16,6 @@ program
   .description('Check which files and directories in project are ignored')
   .usage('<cmd>');
 
-program.on('command:*', (commands?: string[]) => {
-  if (commands) {
-    console.error(`error: unknown command: ${commands[0]}`);
-
-    process.exit(1);
-  }
-});
-
 program.on('--help', () => {
   console.log(
     chalk.greenBright(
@@ -33,7 +25,3 @@ program.on('--help', () => {
 });
 
 program.parse(process.argv);
-
-if (!process.argv.slice(2).length) {
-  program.help();
-}
